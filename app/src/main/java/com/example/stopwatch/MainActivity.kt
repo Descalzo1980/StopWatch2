39f753c61b83c2a71536f333414886924c65e7dc
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-10-04 21:33:21 +0300

    add ViewBinding

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.stopwatch
 
 import android.os.Bundle
 import android.os.SystemClock
-import android.widget.Button
-import android.widget.Chronometer
 import androidx.appcompat.app.AppCompatActivity
+import com.example.stopwatch.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var stopwatch : Chronometer
+    private lateinit var binding: ActivityMainBinding
+
     private var running = false
     private var offset: Long = 0
 
@@ -18,72 +18,73 @@ class MainActivity : AppCompatActivity() {
     private val RUNNING_KEY = "running"
     private val BASE_KEY = "base"
 
-        override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        stopwatch = findViewById(R.id.stopwatch)
-        if(savedInstanceState !=null){
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        if (savedInstanceState != null) {
             offset = savedInstanceState.getLong(OFFSET_KEY)
             running = savedInstanceState.getBoolean(RUNNING_KEY)
-            if(running){
-                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
-                stopwatch.start()
-            }else setBaseTime()
+            if (running) {
+                binding.stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                binding.stopwatch.start()
+            } else setBaseTime()
         }
-        val startWatch = findViewById<Button>(R.id.btn_start)
-        startWatch.setOnClickListener{
-            if(!running){
+        binding.btnStart.setOnClickListener {
+            if (!running) {
                 setBaseTime()
-                stopwatch.start()
+                binding.stopwatch.start()
                 running = true
             }
         }
 
-        val pauseWatch = findViewById<Button>(R.id.btn_pause)
-        pauseWatch.setOnClickListener{
-            if(running){
+        binding.btnPause.setOnClickListener {
+            if (running) {
                 saveOffset()
-                stopwatch.stop()
+                binding.stopwatch.stop()
                 running = false
             }
         }
 
-        val resetButton = findViewById<Button>(R.id.btn_reset)
-        resetButton.setOnClickListener{
+        binding.btnReset.setOnClickListener {
             offset = 0
             setBaseTime()
         }
     }
+
     override fun onRestart() {
         super.onRestart()
-        if(running){
+        if (running) {
             setBaseTime()
-            stopwatch.start()
+            binding.stopwatch.start()
             offset = 0
         }
     }
 
     override fun onStop() {
         super.onStop()
-        if(running){
+        if (running) {
             saveOffset()
-            stopwatch.stop()
+            binding.stopwatch.stop()
         }
     }
 
     override fun onSaveInstanceState(savedInstanceState: Bundle) {
-        savedInstanceState.putLong(OFFSET_KEY,offset)
-        savedInstanceState.putBoolean(RUNNING_KEY,running)
-        savedInstanceState.putLong(BASE_KEY,stopwatch.base)
+        savedInstanceState.putLong(OFFSET_KEY, offset)
+        savedInstanceState.putBoolean(RUNNING_KEY, running)
+        savedInstanceState.putLong(BASE_KEY, binding.stopwatch.base)
         super.onSaveInstanceState(savedInstanceState)
     }
 
-    private fun setBaseTime(){
-        stopwatch.base = SystemClock.elapsedRealtime()-offset
+    private fun setBaseTime() {
+        binding.stopwatch.base = SystemClock.elapsedRealtime() - offset
 
     }
 
-    private fun saveOffset(){
-        offset = SystemClock.elapsedRealtime()-stopwatch.base
+    private fun saveOffset() {
+        offset = SystemClock.elapsedRealtime() - binding.stopwatch.base
     }
 }
